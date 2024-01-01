76720e92439ea7bcf8c69b9746338e19ef832cb7
jjjiyoung0130 <jiyoung63@nate.com>
2024-01-02 00:04:12 +0900

    feat(jjjiyoung) 01/02 : 최근 검색어 기능 구현

--- Javascript/lastflow_search/src/pages/main/main.js
+++ Javascript/lastflow_search/src/pages/main/main.js
@@ -1,24 +1,46 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const MainPage = () => {
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [error, setError] = useState(null);
+  const [recentSearch, setRecentSearch] = useState([]);
 
+  useEffect(() => {
+    const storedSearches = localStorage.getItem("recentSearches");
+    if (storedSearches) {
+      setRecentSearch(JSON.parse(storedSearches));
+    }
+  }, []);
+
+  const savedRecentSearch = async (searches) => {
+    localStorage.setItem("recentSearches", JSON.stringify(searches));
+  };
+
+  const updateSearches = [
+    search,
+    ...recentSearch.filter((item) => item !== search),
+  ];
+  const limitedRecentSearches = updateSearches.slice(0, 5);
   const handleSearch = async () => {
     try {
       const response = await axios.get(
         `http://localhost:3000/search?key=${search}`
       );
-
       console.log(response.data);
-      setSearchResult(response.data);
+      const newSearchResult = response.data;
+
+      setSearchResult(newSearchResult);
+      setRecentSearch(limitedRecentSearches);
+      savedRecentSearch(limitedRecentSearches);
       setError(null);
     } catch (error) {
       console.log("serching error", error);
       if (error.response) {
         setError(error.response.data);
+        setRecentSearch(limitedRecentSearches);
+        savedRecentSearch(limitedRecentSearches);
       }
       setSearchResult([]);
     }
@@ -42,8 +64,13 @@ const MainPage = () => {
             ))}
           </ul>
         )}
-        <div></div>
       </div>
+      <p>최근 검색어</p>
+      <ul>
+        {recentSearch.map((recent, index) => (
+          <li key={index}>{recent}</li>
+        ))}
+      </ul>
     </>
   );
 };
