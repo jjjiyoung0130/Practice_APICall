ff5b261ec11350409ae4c6164432b27089969bab
jjjiyoung0130 <jiyoung63@nate.com>
2024-01-03 23:57:01 +0900

    feat(jjjiyoung0130) 01/03 : input 클릭 시 최근 검색어 보이게 구현 중

--- Javascript/lastflow_search/src/layout/Header.js
+++ Javascript/lastflow_search/src/layout/Header.js
@@ -1,4 +1,16 @@
+import styled from "styled-components";
+
 const Header = () => {
-  return <h1>Search</h1>;
+  return (
+    <Styled.Wrapper>
+      <h1>Search</h1>
+    </Styled.Wrapper>
+  );
 };
 export default Header;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Styled = { Wrapper };
--- Javascript/lastflow_search/src/pages/main/main.js
+++ Javascript/lastflow_search/src/pages/main/main.js
@@ -1,11 +1,15 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { styled } from "styled-components";
 
 const MainPage = () => {
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [error, setError] = useState(null);
   const [recentSearch, setRecentSearch] = useState([]);
+  const [showRecentSearches, setShowRecentSearches] = useState(false);
+
+  const wrapperRef = useRef(null);
 
   useEffect(() => {
     const storedSearches = localStorage.getItem("recentSearches");
@@ -45,33 +49,157 @@ const MainPage = () => {
       setSearchResult([]);
     }
   };
+
+  const handleInputClick = () => {
+    setShowRecentSearches(true);
+    console.log(showRecentSearches);
+  };
+
+  /* const handleOutsideClick = (e) => {
+    // 현재 요소를 포함한 가장 가까운 조상 요소 중 클래스가 "recentSearchContainer"인 요소를 찾고 없으면 false
+    if (e.target.closest(".recentSearchContainer") === null) {
+      setShowRecentSearches(false);
+    }
+  }; */
+  /* const handleOutsideClick = (e) => {
+    if (e.target.closest(".recentSearchContainer") === null) {
+      setShowRecentSearches((prev) => {
+        // 현재 상태 값에 따라 업데이트
+        return prev ? false : prev;
+      });
+    }
+  }; */
+  const handleOutsideClick = (e) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(e.target)) {
+      setShowRecentSearches((prev) => !prev);
+      console.log(showRecentSearches);
+    }
+  };
+
+  useEffect(() => {
+    if (showRecentSearches) {
+      document.addEventListener("click", handleOutsideClick);
+    } else {
+      document.removeEventListener("click", handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [showRecentSearches]);
+
   return (
-    <>
-      <input
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="검색어를 입력해주세요."
-      />
-      <button onClick={handleSearch}>검색</button>
+    <Styled.Wrapper>
+      <div>
+        <Styled.SearchInput
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onClick={handleInputClick}
+          placeholder="검색어를 입력해주세요."
+        />
+        <Styled.SerachButton onClick={handleSearch}>검색</Styled.SerachButton>
+      </div>
+
+      <Styled.RecentSearchContainer
+        $show={showRecentSearches}
+        className="recentSearchContainer"
+      >
+        {showRecentSearches && (
+          <div>
+            <h5>최근 검색어</h5>
+            <ul>
+              {recentSearch.map((recent, index) => (
+                <Styled.RecentSearch key={index}>{recent}</Styled.RecentSearch>
+              ))}
+            </ul>
+          </div>
+        )}
+      </Styled.RecentSearchContainer>
 
       <div>
         {error ? (
           <p>{error}</p>
         ) : (
-          <ul>
+          <Styled.ResultContents>
+            <h4>검색결과</h4>
             {searchResult.map((search, index) => (
-              <li key={index}>{search}</li>
+              <Styled.SearchResult key={index}>{search}</Styled.SearchResult>
             ))}
-          </ul>
+          </Styled.ResultContents>
         )}
       </div>
-      <p>최근 검색어</p>
-      <ul>
-        {recentSearch.map((recent, index) => (
-          <li key={index}>{recent}</li>
-        ))}
-      </ul>
-    </>
+    </Styled.Wrapper>
   );
 };
 export default MainPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SearchInput = styled.input`
+  width: 330px;
+  padding: 10px;
+  border: 2px solid #365486;
+  border-radius: 10px;
+`;
+
+const SerachButton = styled.button`
+  padding: 10px;
+  margin-left: 10px;
+  background-color: #86b6f6;
+  border: 1px solid #86b6f6;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+`;
+
+const ResultContents = styled.ul`
+  width: 400px;
+  border: 1px solid #365486;
+  border-radius: 20px;
+  padding: 10px;
+`;
+
+const SearchResult = styled.li`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  color: ${(props) =>
+    props.search && props.children.includes(props.search)
+      ? "#ffff99"
+      : "black"};
+`;
+
+const RecentSearchContainer = styled.div`
+  width: 300px;
+  border: 1px solid #365486;
+  border-radius: 10px;
+  display: ${(props) => (props.$show ? "inline-block" : "none")};
+  position: absolute;
+  background-color: white;
+  z-index: 1;
+`;
+
+const RecentSearch = styled.li`
+  list-style: none;
+  padding: 10px;
+  margin: 0;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`;
+
+const Styled = {
+  Wrapper,
+  SearchInput,
+  SerachButton,
+  ResultContents,
+  SearchResult,
+  RecentSearchContainer,
+  RecentSearch,
+};
